841783420606813f9d98e442949e773e4061ff8e
Vadim Chernenko <jskiddy@yandex.ru>
2015-09-14 23:32:18 +0300

    Cache.addAll() polyfill

--- sw.js
+++ sw.js
@@ -8,6 +8,33 @@ var FILES = [
     VERSION = 'ver1',
     CACHENAME = PREFIX + VERSION;
 
+Cache.prototype.addAll =
+Cache.prototype.addAll || function addAll(requests) {
+    var cache = this;
+
+    return Promise
+        .resolve()
+        .then(function () {
+            return Promise.all(
+                requests.map(function (request) {
+                    if (!(request instanceof Request)) {
+                        request = new Request(String(request));
+                    }
+
+                    return fetch(request.clone());
+                })
+            );
+        })
+        .then(function (responses) {
+            return Promise.all(
+                responses.map(function (response, i) {
+                    return cache.put(requests[i], response);
+                })
+            );
+        })
+        .then(function () {});
+};
+
 this.addEventListener('fetch', function (event) {
     event.respondWith(
         caches
